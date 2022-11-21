d77d1c80b8c927b96a95c0f00a968d6cc78add5d
Emily <emelizadaniels@gmail.com>
2022-11-21 10:02:12 -0500

    commit tree 65dfa5b8803cc41b1a0b0c610409dafceefb3965

--- looping.rb
+++ looping.rb
@@ -1,8 +1,17 @@
+
+
 def happy_new_year
-  # your code here
+ num = 10 
+ while num <= 10 
+  puts num
+  if num == 0
+    puts "Happy New Year!"
+ end 
 end
 
+
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
+
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
@@ -16,9 +25,30 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+(1..100).each do |num|
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
+
 end
 
 def reverse_string(str)
-  # your code here
+  reversed_string = ''
+  i = 0
+  while i < str.length
+    reversed_string = str[i] + reversed_string
+    i=+ 1
 end
+end
+# This method is saying:
+# create a variable to hold our new reversed String
+# index = 0
+# while the index we are iterating over is less that the lenght of our String
+# we are adding the iterated letter to the front of the sting
+# we keep going, one at a time
